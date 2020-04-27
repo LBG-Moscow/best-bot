d884bb0a24e4595bdf18e502964d26b7e6a35bd8
Caio F <caio.fleury.r@gmail.com>
2020-04-27 18:07:04 +0300

    Fixed wrong import

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
-const { Client } = require('pg');
+const PG_Client = require('pg');
 DB_config = require('./db_config.json');
 const DB_URL = process.env.DATABASE_URL || DB_config.URL;
 
 //This will error out if you do not have the database_url or in your config file.
-const PGclient = new Client({
+const PGclient = new PG_Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
 });
